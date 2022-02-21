e1f85bc82586af5dd3888c5a084b9695e5cfca37
rabdugali <abdugali.rakhymzhan@gmail.com>
2022-02-21 20:19:37 +0600

    Add main menu view

--- Express Order/View/MainViewController.swift
+++ Express Order/View/MainViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(MainMenuTableViewCell.nib, forCellReuseIdentifier: MainMenuTableViewCell.identifier)
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MainMenuTableViewCell.identifier) as! MainMenuTableViewCell
+        cell.configureCell(image: UIImage(named: "testImage"), title: "Del Papa", street: "ул. Бухар жырау, 66, уг. ул. Ауэзова")
+        return cell
     }
-    */
-
 }
