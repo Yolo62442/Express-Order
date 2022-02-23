be2034cc3af8ad6d6aca0433559f4ef08ee03429
Ainura <ainura@MacBook-Air-Ainura.local>
2022-02-23 20:02:41 +0600

    initializing Details

--- Express Order/View/OrderDetailsViewController.swift
+++ Express Order/View/OrderDetailsViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class OrderDetailsViewController: UIViewController {
 
+    @IBOutlet weak var cafeTitleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var orderNumLabel: UILabel!
+    @IBOutlet weak var orderListTableView: UITableView!
+    var cellId  = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*configureTableView()*/
+    }
 
-        // Do any additional setup after loading the view.
+/*        func configureTableView(){
+            orderListTableView.delegate = self
+            orderListTableView.dataSource = self
+            orderListTableView.register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
+               
+           }
+}
+
+extension OrderDetailsViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }*/
+    
+    
 }
